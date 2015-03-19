42930dbc119154700a5b6916a94393fa57aae052
Michael <mike-bailey@users.noreply.github.com>
2015-03-18 22:21:29 -0400

    Update setupenv.sh

--- setupenv.sh
+++ setupenv.sh
@@ -6,4 +6,7 @@ else
 	echo You have 1GB RAM or more. Good stuff.
 fi
 ufw disable
+apt-get update -y && apt-get upgrade -y
 apt-get install nmap -y &> /dev/null
+apt-get install smbclient -y &> /dev/null
+apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev openjdk-7-jre git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev -y
